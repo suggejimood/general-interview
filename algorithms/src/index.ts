b8f7258ea41e6b44f5442397495c13dca0c953df
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-07-07 23:33:49 +0300

    feat: Main index file created

---
+++ algorithms/src/index.ts
@@ -0,0 +1,19 @@
+import {
+  arrayTwoSum,
+  firstUniqueChar,
+  validAnagram,
+  validPalindrome,
+} from "./array/index";
+
+function main() {
+  console.log("--- Now Start ---");
+
+  arrayTwoSum();
+  firstUniqueChar();
+  validAnagram();
+  validPalindrome();
+
+  console.log("--- The End ---");
+}
+
+main();
