6af3beb42f781eb8e9a25d0f05aaf8f07686e961
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-07-08 11:06:35 +0300

    refactor: Top-k refactor

--- interview-questions/src/hash-map/top-k.ts
+++ interview-questions/src/hash-map/top-k.ts
@@ -1,24 +1,24 @@
 import { performance } from "perf_hooks";
 
 //BigO -> O(n logn)
-function ans1(nums: number[], k: number): void {
-  const startTime = performance.now();
-
+function ans1(nums: number[], k: number): number[] {
   const map = new Map<number, number>();
 
-  for (let n of nums) {
+  for (const n of nums) {
     map.set(n, (map.get(n) || 0) + 1);
   }
 
-  const sorted = Array.from(map.entries()).sort((a, b) => b[1] - a[1]);
-
-  const result = sorted.slice(0, k).map((entry) => entry[0]);
-
-  const endTime = performance.now();
-  console.log("Answer: " + result);
-  console.log(`Performance Time ${endTime - startTime} ms`);
+  return [...map.entries()]
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, k)
+    .map(([num]) => num);
 }
 
 export function topK(): void {
-  ans1([1, 1, 1, 2, 2, 3], 2);
+  console.log("-top k");
+  const startTime = performance.now();
+  const result = ans1([1, 1, 1, 2, 2, 3], 2);
+  const endTime = performance.now();
+  console.log("Answer: " + result);
+  console.log(`Performance Time ${endTime - startTime} ms`);
 }
